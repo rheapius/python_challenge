6b28816927562eddbc18176c7d66426c2a26f77f
rheapius <148353935+rheapius@users.noreply.github.com>
2023-12-07 15:19:33 +0800

    Pybank final

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,29 +4,49 @@ import csv
 pybank= os.path.join('/users/rheapius/python_challenge/pybank/resources/budget_data.csv')
 
 months=[]
-ProfitLoss=[]
+Profits=[]
+
+def sum(lst):
+    total=0
+    for number in lst:
+        total += int(number)
+    return total
 
 with open(pybank, 'r') as csvfile:
     csvreader= csv.reader(csvfile, delimiter=',')
     header= next(csvreader)
-    for range in csvreader:
-        months.append(range[0])
-        ProfitLoss.append(range[1])
+    for row in csvreader:
+        months.append(row[0])
+        Profits.append(row[1])
+    
+    total_months= int(len(months))
+    net_profit= int(sum(Profits))
+    average= round(int(net_profit/total_months))
+
+    Greatest_increase= int(max(Profits))
+    Greatest_decrease= int(min(Profits))
+    
+    Output= (
+        f"Financial Analysis\n"
+        f"------------------------------------------------------\n"   
+        f"Total Months: {total_months}\n"
+        f"Total: $ {net_profit}\n"
+        f"Average Change:$ {average}\n"
+        f"Greatest increase in profits: $ ({Greatest_increase})\n"
+        f"Greatest decrease in profits: $ ({Greatest_decrease})\n"
+        )
+print (Output)
+
+results= os.path.join('/users/rheapius/python_challenge/pybank/analysis/results.txt')
+
+with open(results, "w") as text_file:
+    text_file.write(Output)
+
+
+
+
+    
         
-        total_months= int(len(months))
-        total= int(float(sum(ProfitLoss)))
-        average= round(int(float(total/total_months)))
-
-        Greatest_increase= int(float(max(ProfitLoss)))
-
-        Greatest_decrease= int(float(min(ProfitLoss)))
-
-        print(f"------------------------------------------------------")
-        print(f"Total Months: {str(total_months)}")
-        print(f"Total: {str(total)}")
-        print(f"Average Change:{str(average)}")
-        print(f"Greatest increase in profits: $ {str(Greatest_increase)}")
-        print(f"Greatest decrease in profits: {str(Greatest_decrease)}")
                                           
 
 
